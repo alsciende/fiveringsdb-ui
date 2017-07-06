f454f4beb889da3dc31e6e41c79a5717016d8513
Alsciende <bertolini.cedric@me.com>
2017-07-06 07:48:31 +0200

    Moved illustrator from Card to PackCard; improved serialization

--- src/services/storeService.js
+++ src/services/storeService.js
@@ -7,17 +7,44 @@ Vue.use(VueResource);
 
 const stores = {};
 const resources = ['cards', 'cycles', 'packs'];
-const labels = {};
 
+/**
+ * load a resource from the server and creates a TAFFY db with the records
+ */
 const getResource = resource => Vue.http.get(`${configService.apiBaseUrl}/api/v1/${resource}`)
   .then((response) => {
     stores[resource] = taffy(response.body.records);
-  });
+  })
+  ;
 
-const load = () => Promise.all(resources.map(getResource));
+/**
+ * add to each Card record 2 properties, packs and cycles, with the codes of each pack and cycle containing the card
+ */
+const denormalizeCards = () => {
+  stores.cards().each(function (record) {
+    record.packs = record.pack_cards.reduce(
+      (packs, pack_card) => {
+        packs[pack_card.pack.code] = pack_card.quantity;
+        return packs;
+      },
+      {}
+    );
+    record.cycles = Object.keys(record.packs).reduce(
+      (cycles, pack_code) => {
+        cycles[stores.packs({ code: pack_code }).first().cycle.code] = 1;
+        return cycles;
+      },
+      {}
+    );
+    stores.cards.merge(record, false);
+  })
+};
+
+const load = () => Promise
+  .all(resources.map(getResource))
+  .then(denormalizeCards);
 
 export default {
   load,
   stores,
-  labels,
 };
