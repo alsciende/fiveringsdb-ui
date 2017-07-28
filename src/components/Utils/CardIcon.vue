0b0cb94da809c7ad8f6e4368a32936a3e65f8650
Alsciende <bertolini.cedric@me.com>
2017-07-28 11:32:39 +0200

    saturated clan colors for icons

--- src/components/Utils/CardIcon.vue
+++ src/components/Utils/CardIcon.vue
@@ -30,12 +30,12 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-    .fg-crab { color: rgb(122,126,149); }
-    .fg-crane { color: rgb(165,200,222); }
-    .fg-dragon { color: rgb(97,144,129); }
-    .fg-lion { color: rgb(203,180,101); }
-    .fg-phoenix { color: rgb(122,126,149); }
-    .fg-scorpion { color: rgb(154,70,57); }
-    .fg-unicorn { color: rgb(122,97,152); }
+    .fg-crab { color: rgb(0,28,148); }
+    .fg-crane { color: rgb(0,137,222); }
+    .fg-dragon { color: rgb(0,164,114); }
+    .fg-lion { color: rgb(203,157,0); }
+    .fg-phoenix { color: rgb(193,100,0); }
+    .fg-scorpion { color: rgb(166,22,0); }
+    .fg-unicorn { color: rgb(120,0,152); }
     .fg-neutral { color: rgb(168,169,171); }
 </style>
