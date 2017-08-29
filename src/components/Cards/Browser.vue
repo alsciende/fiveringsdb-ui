623b03d7a228361b2f0b0c0b3746fa08e83d3696
Alsciende <bertolini.cedric@me.com>
2017-08-29 18:36:10 +0200

    fix route navigation in card search

--- src/components/Cards/Browser.vue
+++ src/components/Cards/Browser.vue
@@ -3,49 +3,20 @@
         <form>
             <div class="d-sm-flex">
                 <div class="form-group mr-2" style="flex:1">
-                    <form v-on:submit.prevent="navigate">
+                    <form v-on:submit.prevent="search">
                         <input type="text" class="form-control" v-model="currentQuery" placeholder="Enter query">
                     </form>
                 </div>
-                <div class="btn-group align-self-start mr-2" data-toggle="buttons">
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentView === 'table' ? 'active' : '']"
-                           v-on:click="currentView = 'table'" title="View as Table">
-                        <input type="radio" name="currentView" id="table" autocomplete="off" v-model="currentView">
-                        <span class="fa fa-list"></span>
-                    </label>
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentView === 'text' ? 'active' : '']"
-                           v-on:click="currentView = 'text'" title="View as Text">
-                        <input type="radio" name="currentView" id="text" autocomplete="off" v-model="currentView">
-                        <span class="fa fa-th"></span>
-                    </label>
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentView === 'image' ? 'active' : '']"
-                           v-on:click="currentView = 'image'" title="View as Image">
-                        <input type="radio" name="currentView" id="image" autocomplete="off" v-model="currentView">
-                        <span class="fa fa-file-image-o"></span>
-                    </label>
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentView === 'full' ? 'active' : '']"
-                           v-on:click="currentView = 'full'" title="View as Both">
-                        <input type="radio" name="currentView" id="full" autocomplete="off" v-model="currentView">
-                        <span class="fa fa-file-text"></span>
-                    </label>
-                </div>
-                <div class="btn-group align-self-start mr-2" data-toggle="buttons">
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentSort === 'name' ? 'active' : '']"
-                           v-on:click="currentSort = 'name'" title="Sort by Name">
-                        <input type="radio" name="currentSort" id="name" autocomplete="off" v-model="currentSort">
-                        <span class="fa fa-font"></span>
-                    </label>
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentSort === 'clan' ? 'active' : '']"
-                           v-on:click="currentSort = 'clan'" title="Sort by Clan">
-                        <input type="radio" name="currentSort" id="clan" autocomplete="off" v-model="currentSort">
-                        <span class="fa fa-flag"></span>
-                    </label>
-                    <label v-bind:class="['btn', 'btn-outline-secondary', currentSort === 'type' ? 'active' : '']"
-                           v-on:click="currentSort = 'type'" title="Sort by Type">
-                        <input type="radio" name="currentSort" id="type" autocomplete="off" v-model="currentSort">
-                        <span class="fa fa-cube"></span>
-                    </label>
-                </div>
+                <b-form-radio class="mb-4 mr-2"
+                              button-variant="outline-secondary"
+                              buttons
+                              v-model="view"
+                              :options="viewOptions"></b-form-radio>
+                <b-form-radio class="mb-4 mr-2"
+                              button-variant="outline-secondary"
+                              buttons
+                              v-model="sort"
+                              :options="sortOptions"></b-form-radio>
                 <div class="btn-group align-self-start">
                     <b-btn v-b-toggle.searchHelp variant="outline-secondary">
                         <span class="fa fa-info-circle"></span>
@@ -59,19 +30,20 @@
         <b-pagination
                 v-bind:total-rows="totalRows"
                 v-bind:per-page="perPage"
-                v-model="currentPage"
+                v-model="page"
                 size="md"
                 class="my-3 justify-content-center"
         >
         </b-pagination>
-        <cards-list-full v-if="currentView === 'full' || totalRows === 1" v-bind:cards="cards"></cards-list-full>
-        <cards-list-text v-else-if="currentView === 'text'" v-bind:cards="cards"></cards-list-text>
-        <cards-list-image v-else-if="currentView === 'image'" v-bind:cards="cards"></cards-list-image>
-        <cards-list-table v-else-if="currentView === 'table'" v-bind:cards="cards"></cards-list-table>
+        <cards-list-full v-if="view === 'full' || totalRows === 1"
+                         v-bind:cards="paginatedList"></cards-list-full>
+        <cards-list-text v-else-if="view === 'text'" v-bind:cards="paginatedList"></cards-list-text>
+        <cards-list-image v-else-if="view === 'image'" v-bind:cards="paginatedList"></cards-list-image>
+        <cards-list-table v-else-if="view === 'table'" v-bind:cards="paginatedList"></cards-list-table>
         <b-pagination
                 v-bind:total-rows="totalRows"
                 v-bind:per-page="perPage"
-                v-model="currentPage"
+                v-model="page"
                 size="md"
                 class="my-3 justify-content-center"
         >
@@ -110,25 +82,65 @@
     },
     data() {
       return {
-        cards: [],
+        realQuery: this.query,
         currentQuery: this.query,
-        totalRows: 0,
-        currentPage: 1,
-        result: [],
-        currentView: 'table', // text | image | full | table
-        currentSort: 'name', // name | clan | type
         searchHelp: queryMapper.formatAsHtml(),
-        params: {},
+        page: 1,
+        view: 'table',
+        sort: 'name',
+        viewOptions: [
+          {
+            value: 'table',
+            text: '<span class="fa fa-list"></span>',
+          },
+          {
+            value: 'text',
+            text: '<span class="fa fa-th"></span>',
+          },
+          {
+            value: 'image',
+            text: '<span class="fa fa-file-image-o"></span>',
+          },
+          {
+            value: 'full',
+            text: '<span class="fa fa-file-text"></span>',
+          },
+        ],
+        sortOptions: [
+          {
+            value: 'name',
+            text: '<span class="fa fa-font"></span>',
+          },
+          {
+            value: 'clan',
+            text: '<span class="fa fa-flag"></span>',
+          },
+          {
+            value: 'type',
+            text: '<span class="fa fa-cube"></span>',
+          },
+        ],
       };
     },
     computed: {
-      perPage: function () {
-        switch (this.currentView) {
+      filteredList() {
+        const clauses = queryParser.parse(this.realQuery);
+        const queryInput = new QueryInput(clauses);
+        const filters = queryBuilder.build(queryInput);
+        return stores.cards.apply(this, filters).order(this.sort).get();
+      },
+      totalRows() {
+        return this.filteredList.length;
+      },
+      paginatedList() {
+        return this.filteredList.slice((this.page - 1) * this.perPage, this.page * this.perPage);
+      },
+      perPage() {
+        switch (this.view) {
           case 'table':
           case 'text':
             return 300;
-          case 'image':
-          case 'full':
+          default:
             return 20;
         }
       },
@@ -137,63 +149,40 @@
       const params = parseRouteQuery(to);
       next((vm) => {
         vm.currentQuery = params.q;
-        vm.currentPage = params.page;
-        vm.currentView = params.view;
-        vm.currentSort = params.sort;
-        vm.params = params;
-        vm.filter();
+        vm.realQuery = params.q;
+        vm.page = params.page;
+        vm.view = params.view;
+        vm.sort = params.sort;
       });
     },
-    beforeRouteUpdate(to, from, next) {
-      const params = parseRouteQuery(to);
-      this.currentQuery = params.q;
-      this.currentPage = params.page;
-      this.currentView = params.view;
-      this.currentSort = params.sort;
-      this.params = params;
-      this.filter();
-      next();
-    },
     watch: {
-      currentPage(page) {
-        if (page !== null) {
-          this.navigate();
-        }
+      page() {
+        this.navigate();
       },
-      currentView(view) {
-        if (view !== null) {
-          this.navigate();
-        }
+      view() {
+        this.navigate();
       },
-      currentSort(sort) {
-        if (sort !== null) {
-          this.navigate();
-        }
+      sort() {
+        this.navigate();
       },
     },
     methods: {
-      filter() {
-        const clauses = queryParser.parse(this.currentQuery);
-        const queryInput = new QueryInput(clauses);
-        const filters = queryBuilder.build(queryInput);
-        this.result = stores.cards.apply(this, filters).order(this.currentSort).get();
-        this.totalRows = this.result.length;
-        this.cards = this.result.slice((this.currentPage - 1)
-          * this.perPage, this.currentPage * this.perPage);
-        window.scrollTo(0, 0);
+      search() {
+        this.realQuery = this.currentQuery;
+        this.navigate();
       },
       navigate() {
-        const route = queryRouter.getRoute(this.currentQuery);
+        const route = queryRouter.getRoute(this.realQuery);
         if (route.query === undefined) {
           route.query = { q: '' };
         }
-        route.query.page = this.currentPage;
-        route.query.view = this.currentView;
-        route.query.sort = this.currentSort;
-        if (JSON.stringify(route.query) === JSON.stringify(this.params)) {
-          return;
-        }
+        route.query.page = this.page;
+        route.query.view = this.view;
+        route.query.sort = this.sort;
 
+        if (route.query.q === '') {
+          delete route.query.q;
+        }
         if (route.query.page === 1) {
           delete route.query.page;
         }
