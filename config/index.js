3be709b7e8d315ab6c02d048845171f48c2c9889
Alsciende <bertolini.cedric@me.com>
2017-09-22 14:14:45 +0200

    .env file automatically loaded and validated

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
+require('dotenv-safe').load()
+
 module.exports = {
     build: {
         env: require('./prod.env'),
