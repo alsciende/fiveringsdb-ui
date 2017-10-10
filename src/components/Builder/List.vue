cf4bf53dd6587406b26a583a3de46f03dec6ae64
Alsciende <bertolini.cedric@me.com>
2017-10-10 15:57:40 +0200

    change list of private decks

--- src/components/Builder/List.vue
+++ src/components/Builder/List.vue
@@ -12,39 +12,34 @@
                     >Create new
                     </a>
                 </h1>
-                <table class="table">
-                    <tr v-for="strain in strains">
-                        <td class="selector" hidden>
-                            <input type="checkbox"/>
-                        </td>
-                        <td class="bg-clan-icon" :class="[strain.head ? 'clan-'+strain.head.clan : '']">
-                            {{ strain.head ? strain.head.name : $t('msg.empty_strain') }}
-                            <div class="small text-secondary">{{ strain.created_at | formatDate }}</div>
-                        </td>
-                        <td class="actions">
-                            <button type="button" class="btn btn-outline-secondary btn-sm"
-                                    @click.prevent="view(strain)" v-if="strain.head">View
-                            </button>
-                            <button type="button" class="btn btn-outline-primary btn-sm"
-                                    @click.prevent="start(strain)" v-else>Create
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+                <div class="list-group">
+                    <a v-for="strain in strains"
+                       :key="strain.id"
+                            href="#"
+                            @click.prevent="click(strain)"
+                            class="list-group-item list-group-item-action">
+                        <h5>{{ strain.head ? strain.head.name : $t('msg.empty_strain') }}</h5>
+                        <div class="small text-secondary">{{ strain.created_at | formatDate }}</div>
+                    </a>
+                </div>
             </div>
             <div class="col-lg-6">
                 <div v-if="strain">
                     <h2>{{ strain.head.name }}</h2>
                     <div class="mb-2">
-                        <router-link :to="{ name: 'deck-edit', params: { strainId: strain.id, deckId: strain.head.id } }"
-                                     class="btn btn-outline-primary btn-sm">Edit</router-link>
+                        <router-link
+                                :to="{ name: 'deck-edit', params: { strainId: strain.id, deckId: strain.head.id } }"
+                                class="btn btn-outline-primary btn-sm">Edit
+                        </router-link>
                         <b-btn v-b-modal.modalDelete class="btn btn-outline-danger btn-sm">Delete</b-btn>
                         <router-link :to="{ name: 'strain-view', params: { strainId: strain.id } }"
                                      class="btn btn-outline-info btn-sm" target="_blank">
                             Permalink
                         </router-link>
-                        <router-link :to="{ name: 'deck-publish', params: { strainId: strain.id, deckId: strain.head.id } }"
-                                     class="btn btn-outline-warning btn-sm">Publish</router-link>
+                        <router-link
+                                :to="{ name: 'deck-publish', params: { strainId: strain.id, deckId: strain.head.id } }"
+                                class="btn btn-outline-warning btn-sm">Publish
+                        </router-link>
                     </div>
                     <b-modal id="modalDelete"
                              ref="modalDelete"
@@ -82,6 +77,13 @@
       };
     },
     methods: {
+      click(strain) {
+        if (strain.head) {
+          this.view(strain);
+        } else {
+          this.start(strain);
+        }
+      },
       view(strain) {
         this.strain = strain;
       },
@@ -148,11 +150,11 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.bg-clan-icon {
-    background-repeat: no-repeat;
-    background-size: 30px;
-    background-position-y: center;
-    background-position-x: 90%;
-}
+    .bg-clan-icon {
+        background-repeat: no-repeat;
+        background-size: 30px;
+        background-position-y: center;
+        background-position-x: 90%;
+    }
 
 </style>
